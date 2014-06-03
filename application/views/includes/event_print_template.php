0f12a2ba851cb67eec5f47bc4b6a909171e9d088
Indika Ranasinghe <indikapgis@yahoo.com>
2014-06-03 13:24:07 -0400

    Create event_print_template.php

---
+++ application/views/includes/event_print_template.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Referral - OCC Copy </title>
+</head>
+
+<body>
+<table width="645" border="0" cellspacing="2" cellpadding="2" align="center">
+  <tr>
+    <td align="left"><img src="<?php echo base_url(); ?>images/logo.png" /></td>
+  </tr>
+  <tr>
+    <td align="center" style="font-size:14px; font-family:Verdana, Geneva, sans-serif; color:#630; font-weight:bold;">Ophthalmic Consultant Centres Inc.</td>
+  </tr>
+  
+  <tr>
+    <td style="font-size:12px; font-family:Verdana, Geneva, sans-serif;  "><?php echo $body_massage; ?></td>
+  </tr>
+  
+  <tr>
+    <td>
+    
+    
+<table width="95%" align="left" border="0" cellpadding="0" cellspacing="0" style="font-size:12px;">
+      <tbody>
+        <tr>
+          <td colspan="2" valign="middle" align="left"><strong>Business Hours </strong></td>
+          <td valign="middle" align="left">&nbsp;</td>
+        </tr>
+        <tr></tr>
+        <tr>
+          <td colspan="2" valign="middle" align="center">Monday ~ Friday</td>
+          <td valign="middle" align="left">8:00 AM - 5:00 PM </td>
+        </tr>
+        <tr>
+          <td valign="middle" align="left">&nbsp;</td>
+          <td valign="middle" align="left">&nbsp;</td>
+          <td valign="middle" align="left">&nbsp;</td>
+        </tr>
+        <tr>
+          <td valign="middle" width="9%" align="left">&nbsp;</td>
+          <td valign="middle" width="21%" align="left"><strong>Mississauga</strong></td>
+          <td valign="middle" width="70%" align="left"><strong>Burlington</strong></td>
+        </tr>
+        <tr>
+          <td valign="middle" align="left"> Tel: <br />
+            Fax: </td>
+          <td valign="middle" align="left">905-212-9482<br />
+            905-212-1012<br /></td>
+          <td valign="middle" align="left"> 905-639-1202 <br />
+            905-639-6427</td>
+        </tr>
+        
+        <tr>
+          <td valign="middle" align="left">Email :</td>
+          <td colspan="2" valign="middle" align="left"><a href="mailto:info@retinamd.ca">info@retinamd.ca</a></td>
+        </tr>
+      </tbody>
+    </table>
+
+
+</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td align="left" style="padding-right:15px; font-size:10px; color:#630;">© Ophthalmic Consultant Centres. All Rights Reserved. </td>
+  </tr>
+</table>
+
+</body>
+</html>
